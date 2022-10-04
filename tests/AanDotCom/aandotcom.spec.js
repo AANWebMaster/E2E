22c5fd1aaa97bdf441ec259e0ef6ded1586acd9b
treither <treither@aan.com>
2022-10-04 15:02:08 -0500

    Adding Google Analytics Tests

---
+++ tests/AanDotCom/aandotcom.spec.js
@@ -0,0 +1,26 @@
+const { test, expect } = require("@playwright/test");
+const APP_URLS = require("../../utilities/url.config");
+
+test("AAN Homepage Google Analytics Configuration", async ({ page }) => {
+
+    await page.route(new RegExp(".*gtm\.js.*", "i"), (route, request) => {
+        expect(request.url()).toEqual("https://www.googletagmanager.com/gtm.js?id=GTM-TL7VTW")
+        route.continue();
+    })
+
+    await page.goto(APP_URLS.AANDOTCOM);
+
+});
+
+test("Brain and Life Homepage Google Analytics Configuration", async ({ page }) => {
+
+    await page.route(new RegExp(".*gtm\.js.*", "i"), (route, request) => {
+        expect(request.url()).toEqual("https://www.googletagmanager.com/gtm.js?id=GTM-P3379VF")
+        route.continue();
+    })
+
+    await page.goto(APP_URLS.BRAINANDLIFE);
+
+});
+
+
--- utilities/url.config.js
+++ utilities/url.config.js
@@ -13,6 +13,7 @@ let APP_URLS = {
     AANDOTCOM: 'http://localhost:65200',
     MEMBER_PROFILE: 'http://localhost:52718/MemberProfile',
     MEMBERSHIP_APPLICATION: 'http://localhost:51719/MembershipApplication',
+    BRAINANDLIFE: 'https://www.brainandlife.org/'
 }
 
 if(env === TEST_ENVIRONMENT.DEV) {
@@ -20,12 +21,14 @@ if(env === TEST_ENVIRONMENT.DEV) {
     APP_URLS.AANDOTCOM = 'https://webdev.aan.com'
     APP_URLS.MEMBER_PROFILE = 'https://webdev.aan.com/MemberProfile'
     APP_URLS.MEMBERSHIP_APPLICATION = 'https://webdev.aan.com/MembershipApplication'
+    APP_URLS.BRAINANDLIFE = 'https://webdev.brainandlife.org/'
 
 } else if(env === TEST_ENVIRONMENT.PROD) {
     APP_URLS.FELLOWSHIP = 'https://www.aan.com/Fellowship'
     APP_URLS.AANDOTCOM = 'https://www.aan.com'
     APP_URLS.MEMBER_PROFILE = 'https://www.aan.com/MemberProfile'
     APP_URLS.MEMBERSHIP_APPLICATION = 'https://www.aan.com/MembershipApplication'
+    APP_URLS.BRAINANDLIFE = 'https://www.brainandlife.org/'
 }
 
 module.exports = APP_URLS;
