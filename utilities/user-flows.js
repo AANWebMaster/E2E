78e907025c8382a736bca8f3e5d3361e4b9a8bbc
treither <treither@aan.com>
2022-10-18 10:02:36 -0500

    Adding Authentication Cache

--- utilities/user-flows.js
+++ utilities/user-flows.js
@@ -1,7 +1,16 @@
 const username = process.env.TEST_USERNAME;
 const password = process.env.TEST_PASSWORD;
 
+let cookieCache;
+
 async function userLogin(page) {
+
+    if(cookieCache != null) {
+      console.debug("Loading user authentication from cache.");
+      page.context().addCookies(cookieCache);
+      return;
+    }
+
     // Go to https://www.aan.com/
     await page.goto('https://www.aan.com/');
   
@@ -23,9 +32,10 @@ async function userLogin(page) {
     // Click text=Email or 6-digit member ID Password Remember me Forgot Password? Log In >> button
     await page.locator('button[type="submit" i] >> text=Log In').click();
     await page.waitForURL('https://www.aan.com/');
-  
+    console.debug("Setting user authentication cache.");
+    cookieCache = await page.context().cookies();
   }
   
   module.exports = {
-    userLogin,
+    userLogin
   }
