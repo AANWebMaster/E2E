b8701c2bc428a2f2a3f43fb306670b0060c5ac49
treither <treither@aan.com>
2022-10-25 09:43:14 -0500

    Adding Redirect to Account Login

--- utilities/user-flows.js
+++ utilities/user-flows.js
@@ -1,21 +1,24 @@
+const APP_URLS = require("./url.config");
+
 const username = process.env.TEST_USERNAME;
 const password = process.env.TEST_PASSWORD;
 
 let cookieCache;
 
-async function userLogin(page) {
+async function userLogin(page, redirectURL) {
 
     if(cookieCache != null) {
       console.debug("Loading user authentication from cache.");
       page.context().addCookies(cookieCache);
+      await page.goto(redirectURL);
       return;
     }
 
     // Go to https://www.aan.com/
-    await page.goto('https://www.aan.com/');
+    await page.goto(APP_URLS.ACCOUNT_LOGIN + redirectURL);
   
     // Click #page-header >> text=Log In
-    await page.locator('#page-header >> text=Log In').click();
+    // await page.locator('#page-header >> text=Log In').click();
   
     // Click [placeholder="Email or 6-digit member ID"]
     await page.locator('#Username').click();
@@ -31,7 +34,7 @@ async function userLogin(page) {
   
     // Click text=Email or 6-digit member ID Password Remember me Forgot Password? Log In >> button
     await page.locator('button[type="submit" i] >> text=Log In').click();
-    await page.waitForURL('https://www.aan.com/');
+    await page.waitForURL(new RegExp(redirectURL, "i"));
     console.debug("Setting user authentication cache.");
     cookieCache = await page.context().cookies();
   }
